e8b1a3d499c7248f4922f2de10074dcea3ebd9d5
Loki Holdercatling <lokih5321@colchester.ac.uk>
2025-12-18 14:52:47 +0000

    new scene controller fr

--- Assets/Scripts/Controllers/SceneController.cs
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -1,15 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public static class SceneController
 {
-    // idea: array of scenes, then nextScene() prevScene() goToScene(), resetScene(go back to start) 
+
+    private static string[] scenes = { "level_1", "SampleScene"};
+    private static int currentSceneIndex = 0;
+
 
     public static bool nextScene()
     {
-        Debug.Log("next scene");
         bool successful;
+
+        currentSceneIndex++;
+
+        if(currentSceneIndex >= scenes.Length)
+        {
+            Debug.Log("Can't move to this scene, there is no screen.");
+            successful = false;
+            return successful;
+        }
+        Debug.Log("next scene");
+        SceneManager.LoadScene(scenes[currentSceneIndex]);
         successful = true;
+        
         return successful;
         
     }
